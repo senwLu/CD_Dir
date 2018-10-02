df6ebee52f1c6a7ea4948a9203e508d79ad383aa
senwLu <swei.lu1018@gmail.com>
2018-10-01 22:25:19 -0700

    finished Pokemon

---
+++ WebFundamentals/API_AJAX/Pokemon/jQuery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    for(let i=1; i < 31; i++) {
+        $('div').append('<img></img>')
+        $('img').last().attr('src','http://pokeapi.co/media/sprites/pokemon/' + i + '.png');
+    }
+});
