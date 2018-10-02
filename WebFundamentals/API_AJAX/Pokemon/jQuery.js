a17228649a09fff9f2900eb664d463a58ceab3dd
senwLu <swei.lu1018@gmail.com>
2018-10-01 22:27:54 -0700

    added interpolation for Pokemon

--- WebFundamentals/API_AJAX/Pokemon/jQuery.js
+++ WebFundamentals/API_AJAX/Pokemon/jQuery.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     for(let i=1; i < 31; i++) {
         $('div').append('<img></img>')
-        $('img').last().attr('src','http://pokeapi.co/media/sprites/pokemon/' + i + '.png');
+        // $('img').last().attr('src','http://pokeapi.co/media/sprites/pokemon/' + i + '.png');
+        $('img').last().attr('src',`http://pokeapi.co/media/sprites/pokemon/${i}.png`)
     }
 });
