dde4b5948034c079e9336a400fb26a5c3edc314a
senwLu <swei.lu1018@gmail.com>
2018-10-02 13:11:29 -0700

    finished pokedex

--- WebFundamentals/API_AJAX/Pokedex/jQuery.js
+++ WebFundamentals/API_AJAX/Pokedex/jQuery.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
-    for(let i=1; i < 51; i++) {
-        $('div.leftDiv').append('<img></img>')
-        $('img').last().attr('src',`http://pokeapi.co/media/sprites/pokemon/${i}.png`)
+    for(let i=1; i < 152; i++) {
+        $('div.leftDiv').append('<img></img>');
+        $('img').last().attr('src',`https://pokeapi.co/media/sprites/pokemon/${i}.png`);
+        $('img').last().attr('id',i);
     }
+
+    $(document).on('click','img', function() {
+        $('div.rightDiv').children().remove();
+        // set variable to pokemon clicked
+        var id = $(this).attr('id');
+
+        
+
+        $.get(`https://pokeapi.co/api/v2/pokemon/${id}/`, function(res) {
+            var name = res.name;
+            var type = res.types[0].type.name;
+            var height = res.height;
+            var weight = res.weight;
+
+            $('.rightDiv').append(`<h2>${name}</h2>`);
+            $('.rightDiv').append(`<img class="centerItems" src=https://pokeapi.co/media/sprites/pokemon/${id}.png></img>`);
+            $('.rightDiv').append('<div id="pokeData"></div>');
+            $('#pokeData').append(`<h4>Types</h4><ul><li>${type}</li></ul>`);
+            $('#pokeData').append(`<h4>Height</h4><p>${height}</p>`);
+            $('#pokeData').append(`<h4>Weight</h4><p>${weight}</p>`);
+
+            
+
+
+            console.log(height);
+        })
+    })
 });
