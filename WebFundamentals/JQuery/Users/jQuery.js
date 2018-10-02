3e336625a37b781439d5a9944b6b2ec51097173e
senwLu <swei.lu1018@gmail.com>
2018-10-01 17:12:26 -0700

    added chain to Users

--- WebFundamentals/JQuery/Users/jQuery.js
+++ WebFundamentals/JQuery/Users/jQuery.js
@@ -5,12 +5,17 @@ $(document).ready(function() {
 
     $('form').submit(function() {
     
-        $('tbody').append("<tr></tr>");
+        // $('table').append("<tr></tr>");
 
-        $('tr').last().append('<td>'+userForm.Fname.value+'</td>'+
-        '<td>'+userForm.Lname.value+'</td>'+
-        '<td>'+userForm.eAddress.value+'</td>'+
-        '<td>'+userForm.contactNo.value+'</td>');
+        // $('tr').last().append('<td>'+userForm.Fname.value+'</td>'+
+        // '<td>'+userForm.Lname.value+'</td>'+
+        // '<td>'+userForm.eAddress.value+'</td>'+
+        // '<td>'+userForm.contactNo.value+'</td>');
+
+        $('table').append('<tr><td>'+ $('input[name="Fname"]').val() + 
+            '</td><td>' + $('input[name="Lname"]').val() + '</td><td>' +
+            $('input[name="eAddress"]').val() + '</td><td>'+
+            $('input[name="contactNo"]').val() + '</td></tr>');
 
         $("#addForm")[0].reset();
 
