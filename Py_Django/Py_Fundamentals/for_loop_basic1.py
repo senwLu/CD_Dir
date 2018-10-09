2362cd29e1a6bf49048c84e4ea7dc493920a6327
senwLu <swei.lu1018@gmail.com>
2018-10-09 01:13:46 -0700

    setup Python and env, als also finished first 3 assignments

---
+++ Py_Django/Py_Fundamentals/for_loop_basic1.py
@@ -0,0 +1,55 @@
+# Print all the numbers/integers from 0 to 150
+# for x in range(0, 151):
+#     print(x)
+
+# Print all the multiples of 5 from 5 to 1,000,000 (used 100)
+# for x in range(1, 100):
+#     x = x*5
+#     print(x)
+
+# Print integers 1 to 100.
+# If divisible by 5, print "Coding" instead.
+# If by 10, also print " Dojo"
+# for x in range(1, 101):
+#     if x % 2 == 0:
+#         print('Coding')
+#     if x % 10 == 0:
+#         print(' Dojo')
+
+# Add odd integers from 0 to 500,000 (used 10), and print the final sum.
+# sum = 0
+# for x in range(0, 10):
+#     if x % 2 != 0:
+#         sum += x
+# print(sum)
+
+# Print positive numbers starting at 2018, counting down by fours (exclude 0)
+# for x in range(2018, 0, -4):
+#     if x > 0:
+#         print(x)
+
+# Based on earlier "Countdown by Fours", given lowNum, highNum, mult,
+# print multiples of mult from lowNum to highNum, using a FOR loop.
+# For (2,9,3), print 3 6 9 (on successive lines)
+# lowNum = 2
+# highNum = 9
+# multi = 3
+
+# for x in range(lowNum, highNum+1):
+#     if x % multi == 0:
+#         print(x)
+
+# list = [3, 5, 1, 2]
+# for i in list:
+#     print(i)
+# 3 5 1 2
+
+# list = [3, 5, 1, 2]
+# for i in range(list):
+#     print(i)
+# TypeError: 'list' object cannot be interpreted as an integer
+
+# list = [3, 5, 1, 2]
+# for i in range(len(list)):
+#     print(i)
+# 0 1 2 3
---
+++ Py_Django/Py_Fundamentals/func_basic_II.py
@@ -0,0 +1,69 @@
+# Countdown - Create a function that accepts a number as an input.
+#   Return a new array that counts down by one, from the number
+#   (as arrays 'zero'th element) down to 0 (as the last element).
+#   For example countDown(5) should return [5,4,3,2,1,0].
+
+
+def countDown(num):
+    arr = []
+    for i in range(num, -1, -1):
+        arr.append(i)
+    return arr
+
+
+# print(countDown(5))
+
+
+# Print and Return - Your function will receive an array with two numbers.
+# Print the first value, and return the second.
+
+def prtRtn(arr):
+    print(arr[0])
+    return arr[1]
+
+
+# print(prtRtn([2, 3]))
+
+# First Plus Length - Given an array, return the sum of the first value in the array,
+#   plus the array's length.
+
+
+def firstPlusLen(arr):
+    sum = arr[0] + len(arr)
+    return sum
+
+
+# print(firstPlusLen([2, 1, 5, 2]))
+
+# Values Greater than Second - Write a function that accepts any array,
+#   and returns a new array with the array values that are greater than its 2nd value.
+#   Print how many values this is.  If the array is only one element long,
+#   have the function return False
+
+def valGreaterSec(arr):
+    rtnArr = []
+    for i in arr:
+        if i > arr[1]:
+            rtnArr.append(i)
+    print(len(rtnArr))
+    if(len(rtnArr) == 1):
+        return False
+    else:
+        return rtnArr
+
+
+# print(valGreaterSec([1, 3, 5, 2, 6]))
+
+# This Length, That Value - Write a function called lengthAndValue which
+#   accepts two parameters, size and value. This function should take two
+#   numbers and return a list of length size containing only the number in value.
+#   For example, lengthAndValue(4,7) should return [7,7,7,7].
+
+def lengthAndValue(size, value):
+    arr = []
+    for i in range(0, size):
+        arr.append(value)
+    return arr
+
+
+# print(lengthAndValue(6, 17))
