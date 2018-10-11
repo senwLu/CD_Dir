a8ed31d5af9d05d3de0c00a97827b5111e813018
senwLu <swei.lu1018@gmail.com>
2018-10-11 12:57:07 -0700

    updated insertionSort

--- Py_Django/Py_Fundamentals/insertionSort.py
+++ Py_Django/Py_Fundamentals/insertionSort.py
@@ -5,13 +5,13 @@ def insertionSort(arr):
     # stop moving back insertion value and insert when insertion value is larger than compared value
     # repeat
 
-    count = 0
-    for i in range(count, len(arr)-1):
-        # if value after current value is smaller
-        if arr[i] > arr[i+1]:
+    # count = 0
+    # for i in range(count, len(arr)-1):
+    #     # if value after current value is smaller
+    #     if arr[i] > arr[i+1]:
 
-            # get where arr[i+1] is in the array
-            start = arr.index(arr[i+1])
+    #         # get where arr[i+1] is in the array
+    #         start = arr.index(arr[i+1])
 
             # loop back towards beginning of array
             # for a in range(start, 0, -1):
@@ -23,16 +23,30 @@ def insertionSort(arr):
 
             ########################################################################
 
-            checker = arr[i+1]
-            # print(checker)
-            # break
-            # keep looping back and insert only when value is larger than next compared value
-            for a in range(start, -1, -1):
-                if checker > arr[a]:
-                    checker = arr[a]
-                    break
+    # loop up the arr
+    for index in range(1, len(arr)):
 
-            # print(checker)
+        # grab value at index and set to currentValue
+        currentValue = arr[index]
+
+        # continue to loop if index is greater than 0
+        # and if currentValue is smaller than the value below it
+        while index > 0 and currentValue < arr[index-1]:
+
+            arr[index] = arr[index-1]
+            index -= 1
+
+        arr[index] = currentValue
+
+    print(arr)
 
 
 insertionSort([4, 8, 5, 2, 3, 1, 6, 9, 7])
+
+# 4, 8, 5, 2
+# 4, 8, 8, 2 => break out of loop => 4, 5, 8, 2
+# 4, 5, 8, 2
+# 4, 5, 8, 8
+# 4, 5, 5, 8
+# 4, 4, 5, 8 => break out of loop 2, 4, 5, 8
+# 2, 4, 5, 8
