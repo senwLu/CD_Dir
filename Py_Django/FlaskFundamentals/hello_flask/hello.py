e87830123306e707965b1c6ed114a12087da42c7
senwLu <swei.lu1018@gmail.com>
2018-10-17 15:46:28 -0700

    modified gitignore and pushing finished work

--- Py_Django/FlaskFundamentals/hello_flask/hello.py
+++ Py_Django/FlaskFundamentals/hello_flask/hello.py
@@ -15,6 +15,52 @@ def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response.
 
 
+@app.route('/success')
+def success():
+    return "success"
+
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+
+@app.route('/say/<strIn>')
+def say(strIn):
+    if strIn == 'john':
+        return 'Hi John!'
+    else:
+        a = list(strIn)
+        a[0] = a[0].upper()
+        a = ''.join(a)
+        return 'Hi ' + a + "!"
+
+
+@app.route('/repeat/<times>/<reStr>')
+def repeat(times, reStr):
+    i = 0
+    strResult = ""
+    while i < int(times):
+        strResult += reStr + " "
+        i += 1
+    return strResult
+
+
+# for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return "hello "+name
+
+
+# for a route '/users/____/____', two parameters in the url get passed as username and id
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+
 # If __name__ is "__main__" we know we are running this file directly and not importing
 # it from a different module
 if __name__ == "__main__":
