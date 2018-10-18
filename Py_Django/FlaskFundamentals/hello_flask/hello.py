43b69d5d32e19b81e5972020378956ce4226d7e4
senwLu <swei.lu1018@gmail.com>
2018-10-17 22:53:34 -0700

    pushing work to repo

--- Py_Django/FlaskFundamentals/hello_flask/hello.py
+++ Py_Django/FlaskFundamentals/hello_flask/hello.py
@@ -1,66 +1,68 @@
-from flask import Flask  # Import Flask to allow us to create our app.
+# Import Flask to allow us to create our app.
+from flask import Flask, render_template
 
 # Global variable __name__ tells Flask whether or not we are running the file
 # directly, or importing it as a module.
 app = Flask(__name__)
 
-print(__name__)          # Just for fun, print __name__ to see what it is
+# print(__name__)          # Just for fun, print __name__ to see what it is
 
 
 # The "@" symbol designates a "decorator" which attaches the following
 @app.route('/')
+def hello_world():
+    return render_template('index.html', name='Jay')
+
+
 # function to the '/' route. This means that whenever we send a request to
 # localhost:5000/ we will run the following "hello_world" function.
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response.
+# def hello_world():
+#     return 'Hello World!'  # Return the string 'Hello World!' as a response.
 
 
-@app.route('/success')
-def success():
-    return "success"
+# @app.route('/success')
+# def success():
+#     return "success"
 
 
-@app.route('/dojo')
-def dojo():
-    return 'Dojo!'
+# @app.route('/dojo')
+# def dojo():
+#     return 'Dojo!'
 
 
-@app.route('/say/<strIn>')
-def say(strIn):
-    if strIn == 'john':
-        return 'Hi John!'
-    else:
-        a = list(strIn)
-        a[0] = a[0].upper()
-        a = ''.join(a)
-        return 'Hi ' + a + "!"
+# @app.route('/say/<strIn>')
+# def say(strIn):
+#     if strIn == 'john':
+#         return 'Hi John!'
+#     else:
+#         a = list(strIn)
+#         a[0] = a[0].upper()
+#         a = ''.join(a)
+#         return 'Hi ' + a + "!"
 
 
-@app.route('/repeat/<times>/<reStr>')
-def repeat(times, reStr):
-    i = 0
-    strResult = ""
-    while i < int(times):
-        strResult += reStr + " "
-        i += 1
-    return strResult
+# @app.route('/repeat/<times>/<reStr>')
+# def repeat(times, reStr):
+#     i = 0
+#     strResult = ""
+#     while i < int(times):
+#         strResult += reStr + " "
+#         i += 1
+#     return strResult
 
 
-# for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
-@app.route('/hello/<name>')
-def hello(name):
-    print(name)
-    return "hello "+name
+# # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+# @app.route('/hello/<name>')
+# def hello(name):
+#     print(name)
+#     return "hello "+name
 
-
-# for a route '/users/____/____', two parameters in the url get passed as username and id
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
-
-
+# # for a route '/users/____/____', two parameters in the url get passed as username and id
+# @app.route('/users/<username>/<id>')
+# def show_user_profile(username, id):
+#     print(username)
+#     print(id)
+#     return "username: " + username + ", id: " + id
 # If __name__ is "__main__" we know we are running this file directly and not importing
 # it from a different module
 if __name__ == "__main__":
