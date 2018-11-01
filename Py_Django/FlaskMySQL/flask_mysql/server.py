d3bad3e86801a4ecca39fceb5a76941cd6ec7fad
senwLu <swei.lu1018@gmail.com>
2018-11-01 01:46:19 -0700

    adding Bcrypt to flask_mysql

--- Py_Django/FlaskMySQL/flask_mysql/server.py
+++ Py_Django/FlaskMySQL/flask_mysql/server.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, redirect, request
 # import the function connectToMySQL from the file mysqlconnection.py
 from mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
+
+# we are creating an object called bcrypt,
+# which is made by invoking the function Bcrypt with our app as an argument
+bcrypt = Bcrypt(app)
+
 # invoke the connectToMySQL function and pass it the name of the database we're using
 # connectToMySQL returns an instance of MySQLConnection, which we will store in the variable 'mysql'
 mysql = connectToMySQL('flaskApp')
@@ -17,13 +24,22 @@ def index():
 
 @app.route('/create_user', methods=['POST'])
 def create():
+
+    # include some logic to validate user input before adding them to the database!
+    # create the hash
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
     mysql = connectToMySQL("flaskApp")
-    query = "INSERT INTO users (username, created_at, updated_at) VALUES (%(username)s, NOW(), NOW());"
+
+    # put the pw_hash in our data dictionary, NOT the password the user provided
     data = {
-        'username': request.form['username']
+        'username': request.form['username'],
+        'password_hash': pw_hash
     }
-    new_user_id = mysql.query_db(query, data)
-    print(new_user_id)
+
+    query = "INSERT INTO users (username, password, created_at, updated_at) VALUES (%(username)s, %(password_hash)s, NOW(), NOW());"
+
+    mysql.query_db(query, data)
 
     return redirect('/')
 
