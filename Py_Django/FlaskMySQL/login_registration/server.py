2a33731f4a20b98ba1ed492553ad3799270a6130
senwLu <swei.lu1018@gmail.com>
2018-11-01 19:25:53 -0700

    pushing more changes to repo

--- Py_Django/FlaskMySQL/login_registration/server.py
+++ Py_Django/FlaskMySQL/login_registration/server.py
@@ -6,8 +6,15 @@ app.secret_key = 'itsSecret'
 
 bcrypt = Bcrypt(app)
 
-# if sessions does not have id, redirect to login page
-# only allow access to login and signup page if no id in session
+
+@app.route('/')
+def index():
+    # if sessions does not have id, redirect to login page
+    # only allow access to login and signup page if no id in session
+    if session.get('user_id') == True:
+        return render_template('index.html')
+    else:
+        return redirect('/login')
 
 
 @app.route('/login')
@@ -18,6 +25,14 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@app.route('/dbList')
+def db_page():
+    if session.get('user_id') == True:
+        return render_template('dbList.html')
+    else:
+        return redirect('/login')
 
 
 if __name__ == '__main__':
